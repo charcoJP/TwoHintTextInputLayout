d4004b365bbc968896ed5f65305db4dd9fc7822e
charcoJP <charco2356@gmail.com>
2019-08-28 21:52:26 +0900

    Add editText to activity_main

--- app/src/main/java/jp/co/charco/twolabeltextinputlayout/MainActivity.kt
+++ app/src/main/java/jp/co/charco/twolabeltextinputlayout/MainActivity.kt
@@ -2,11 +2,22 @@ package jp.co.charco.twolabeltextinputlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val textInputLayout by lazy { findViewById<TextInputLayout>(R.id.textInputLayout) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        if (textInputLayout?.editText?.hasFocus() == true) {
+            textInputLayout.editText!!.clearFocus()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
